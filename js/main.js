607ee622f4e10bfd528324341ca6cd07fc60c514
Evan Dorsky <evan.dorsky@icloud.com>
2021-05-18 23:10:16 -0400

    Fancy in/out transition for detail content

--- js/main.js
+++ js/main.js
@@ -7,10 +7,32 @@
 function keyphrase_click(event) {
   var id = d3.select(this).attr("id")
 
-  var isSelected = d3.select("."+id)
-    .classed("selected")
-  d3.select("."+id)
-    .classed("selected", !isSelected)
+  var sel = "."+id
+
+  var wasSelected = d3.select(sel).classed("selected")
+
+  selector_close(".feature", id)
+  if (!wasSelected) {
+    selector_open(sel, id)
+  }
+}
+
+function selector_open(sel, id) {
+  var els = d3.selectAll(sel)
+  els.style('display', 'block')
+    .style('height', 'auto')
+  var detailHeight = document.querySelector("."+id).offsetHeight
+  els.style('height', 0)
+  setTimeout(function() {
+      els.classed('selected', true)
+        .style('height', detailHeight+'px')
+  }, 5)
+}
+
+function selector_close(sel, id) {
+  var els = d3.selectAll(sel)
+  els.style('height', 0)
+    .classed('selected', false)
 }
 
 function main(event) {
