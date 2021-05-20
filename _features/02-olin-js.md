a588bfae5672a66e7d8bd049ea47f0bd9bc6cc20
Evan Dorsky <evan.dorsky@icloud.com>
2021-05-19 23:29:01 -0400

    New text highlighting, olin.js desc

--- _features/02-olin-js.md
+++ _features/02-olin-js.md
@@ -1,6 +1,8 @@
 ---
 layout: feature
 key: olin-js
-title:  "Apple Special Projects Group"
+title:  "Olin.js"
 ---
-olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs olinjs
+I joined a student teaching team to give the second offering of [Olin.js](https://github.com/olinjs/olinjs), a semester long for-credit course on full stack web development (in [node.js](https://nodejs.org/en/)). We created all the course materials and ran everything – we gave lectures, graded assignments, held office hours, etc.
+
+Our ~25 students (fellow Olin College students) started the semester with some programming experience -- but little to no Javascript experience. By the end of the semester, they were on teams with each other building real web apps using frameworks like [Angular](https://angularjs.org), [Ember](https://emberjs.com), and [React](https://reactjs.org) (all popular in 2015).
--- js/main.js
+++ js/main.js
@@ -7,32 +7,47 @@
 function keyphrase_click(event) {
   var id = d3.select(this).attr("id")
 
-  var sel = "."+id
+  var sel         = "."+id
+  var sel_keyline = "."+id+"-line"
 
   var wasSelected = d3.select(sel).classed("selected")
 
   selector_close(".feature", id)
+  selector_deselect(".key-line")
   if (!wasSelected) {
     selector_open(sel, id)
+    selector_select(sel_keyline)
   }
+}
+
+function selector_select(sel) {
+  var els = d3.selectAll(sel)
+  els.classed('selected', true)
+}
+
+function selector_deselect(sel) {
+  var els = d3.selectAll(sel)
+  els.classed('selected', false)
 }
 
 function selector_open(sel, id) {
   var els = d3.selectAll(sel)
   els.style('display', 'block')
-    .style('height', 'auto')
+     .style('height', 'auto')
   var detailHeight = document.querySelector("."+id).offsetHeight
   els.style('height', 0)
   setTimeout(function() {
-      els.classed('selected', true)
-        .style('height', detailHeight+'px')
+      selector_select(sel)
+
+      els.style('height', detailHeight+'px')
   }, 5)
 }
 
 function selector_close(sel, id) {
   var els = d3.selectAll(sel)
   els.style('height', 0)
-    .classed('selected', false)
+
+  selector_deselect(sel)
 }
 
 function main(event) {
