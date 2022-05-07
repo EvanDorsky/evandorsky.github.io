e07a6077d4d34f34930b4652a537661bf01bd38a
Evan Dorsky <evan.dorsky@icloud.com>
2022-05-07 11:23:49 -0700

    No more flashing in gallery

--- js/main.js
+++ js/main.js
@@ -103,7 +103,7 @@ function reflowGallery() {
   })
 
   var container = d3.select('.wrapper').node()
-  var rowWidth = (container.offsetWidth-32)-1
+  var rowWidth = container.offsetWidth
   im_infos.forEach((row_els, i) => {
     var arSum = 0
     row_els.forEach((el, j) => {
