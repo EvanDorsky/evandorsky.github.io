d01717c046c6908736ce2d06b81fd14e322d7302
Evan Dorsky <evan.dorsky@icloud.com>
2022-09-25 17:20:52 -0700

    New webps!

--- assets/img/film/2021-07-22-alignment/frame.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for n in {1..50}
-do
-convert -gravity Center -extent 2100x2100 "$n"_tn.jpg bordered/"$n"_tn.jpg
-exiftool -overwrite_original -AllDates=now bordered/"$n"_tn.jpg
-done
