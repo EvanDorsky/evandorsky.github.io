ffc5365b1971b6692f70643bd51e5c62dc4e04ef
Evan Dorsky <evan.dorsky@icloud.com>
2023-03-19 15:45:44 -0700

    Don't generate link previews locally

--- _plugins/preview.rb
+++ _plugins/preview.rb
@@ -1,17 +1,18 @@
-Jekyll::Hooks.register :pages, :post_write do |page|
-  # kit.stylesheets << '_site/assets/css/main.css'
-  if page.data["layout"] == "show-stub"
-    path = page.destination("/")
+if not RbConfig::CONFIG["arch"] == "arm64-darwin21"
+  Jekyll::Hooks.register :pages, :post_write do |page|
+    if page.data["layout"] == "show-stub"
+      path = page.destination("/")
 
-    f = File.open(path)
-    fd = f.read
+      f = File.open(path)
+      fd = f.read
 
-    kit = IMGKit.new(fd, 
-      :crop_h => 450)
-    kit.stylesheets << '_site/assets/css/main.css'
-    outpath = path + ".png"
-    t1 = Time.now
-    file = kit.to_file(outpath)
-    t2 = Time.now
+      kit = IMGKit.new(fd, 
+        :crop_h => 450)
+      kit.stylesheets << '_site/assets/css/main.css'
+      outpath = path + ".png"
+      t1 = Time.now
+      file = kit.to_file(outpath)
+      t2 = Time.now
+    end
   end
 end
