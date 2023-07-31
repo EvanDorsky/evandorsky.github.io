8315b4f3e024b09671e5f65c19b9710042d7617a
Evan Dorsky <evan.dorsky@icloud.com>
2023-07-31 10:10:54 -0700

    Fix gallerydate

--- _plugins/gallerydate.rb
+++ _plugins/gallerydate.rb
@@ -2,19 +2,22 @@ module Jekyll
   module GalleryDateFilter
     def gallerydate(datetime, f)
       today = Date.today
-      yesterday = today - 1
+
+      if datetime.is_a?(Integer)
+        datetime = Time.at(datetime)
+      end
 
       date = datetime.to_date
       days_elapsed = (today - date).to_i
 
       if datetime.day == today
         "Today"
-      elsif datetime == yesterday
+      elsif days_elapsed == 1
         "Yesterday"
       elsif days_elapsed < 7
         "#{days_elapsed} days ago"
       elsif datetime.year == today.year
-        datetime.strftime("%b %-d")
+        datetime.strftime(f)
       else
         datetime.year.to_s
       end
