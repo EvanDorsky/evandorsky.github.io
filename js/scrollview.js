4398995125905df80bcfd7e55d2c9e998fee6e32
Evan Dorsky <evan.dorsky@icloud.com>
2025-04-18 00:33:20 -0500

    Messy initial sidebar scroll pin

--- js/scrollview.js
+++ js/scrollview.js
@@ -8,14 +8,14 @@
 function main(event) {
   gsap.registerPlugin(ScrollTrigger)
 
-  // gsap.to(".sidebar.photo-page", {
-  //   scrollTrigger: {
-  //     trigger: ".sidebar.photo-page",
-  //     start: "top top",
-  //     pin: true,
-  //     markers: true
-  //   }
-  // })
+  gsap.to(".scroller-sidebar", {
+    scrollTrigger: {
+      trigger: ".scroller-sidebar",
+      start: "top top",
+      pin: true,
+      // markers: true
+    }
+  })
   // ScrollTrigger.create({
   //   trigger: ".sidebar.photo-page",
   //   start: "top top",
