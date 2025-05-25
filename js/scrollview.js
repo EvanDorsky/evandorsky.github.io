b467b6e09ca732b3a8dd6441878386ae12b8fb88
Evan Dorsky <evan.dorsky@icloud.com>
2025-05-25 16:49:47 -0700

    Scroll by click but it's ridiculous

--- js/scrollview.js
+++ js/scrollview.js
@@ -5,18 +5,24 @@
 )()
 
 function main(event) {
-  // gsap.registerPlugin(ScrollTrigger)
+  d3.selectAll(".pj-list-item")
+    .on("click", (e) => {
+      const slug = e.target.getAttribute("slug")
 
-  // gsap.to(".scroller-sidebar", {
-  //   scrollTrigger: {
-  //     trigger: ".scroller-sidebar",
-  //     start: "top top",
-  //     pin: true,
-  //     // markers: true
-  //   }
-  // })
+      const post = d3.select(`.collection-post-preview-vert[slug="${slug}"]`)
 
+      const scroller = d3.select("html").node()
+      const target = post.node()
+      const targetPos = target.offsetTop - 25
 
-  m.animate(".collection-post-preview-vert", { rotate: 10 })
-
+      const delta = scroller.scrollTop - targetPos
+      console.log('delta')
+      console.log(delta)
+      m.animate(scroller.scrollTop, targetPos, {
+        duration: Math.abs(1e-4 * delta),
+        easing: "ease-in-out",
+        onUpdate: latest => scroller.scrollTop = latest
+      })
+    })
+    
 }
